abcce8bc7e55514544b4bee1b52f587576e778d1
Derrick Nyasio <dnyasio2014@gmail.com>
2022-10-05 17:55:06 +0300

    Delete

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import BotCard from "./BotCard";
 
 function BotCollection({ bots }) {
     // Your code here
+    const [botsUpdated, setbotsUpdated] = useState({ bots })
+
+    function HandleDelete(id) {
+        console.log(id)
+        console.log(botsUpdated)
+
+        const newList = bots.filter((item) => item.id !== id);
+
+        setbotsUpdated(newList);
+        console.log(botsUpdated)
+        fetch(`http://localhost:8002/bots/${id}`, {
+                method: "DELETE",
+            })
+            .then((r) => r.json())
+            .then(() => console.log("deleted!"));
+    }
+
     return ( <
         div className = "ui four column grid" >
         <
         div className = "row" > { /*...and here..*/
             bots.map(bot => ( <
                 BotCard key = { bot.id }
+                id = { bot.id }
                 bot = { bot }
+                handleDelete = { HandleDelete }
                 />
             ))
         }
