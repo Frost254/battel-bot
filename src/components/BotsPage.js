f53fec19453bca86bdf9c640f1c286128a97c220
Derrick Nyasio <dnyasio2014@gmail.com>
2022-10-07 13:14:15 +0300

    Add to Your Army

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -7,19 +7,17 @@ function BotsPage() {
     const [bots, setBots] = useState([]);
     const [botsToPush, setbotsToPush] = useState([bots])
 
-    function HandleAdd(id) {
-        setbotsToPush(bots.filter((item) => item.id === id))
-        console.log("Works")
-        console.log(botsToPush)
-    }
-
-
-    useEffect(() => {
+        useEffect(() => {
         fetch("http://localhost:8002/bots")
             .then((r) => r.json())
             .then((items) => setBots(items));
     }, []);
-
+    
+    function HandleAdd(id) {
+        const botsArray = [];
+        bots.filter(item => (item.id === id ? botsArray.push(item) : ""))
+        setbotsToPush(botsArray)
+    }
 
     return ( 
     <div>
